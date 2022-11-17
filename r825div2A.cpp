407477cba5afd6be8b9a6ba4adcc84c9faa5f145
Jay Chotalia <75968475+Jay1105@users.noreply.github.com>
2022-11-17 09:23:32 +0530

    Create r825div2A.cpp

---
+++ r825div2A.cpp
@@ -0,0 +1,58 @@
+// CF Round 825 Div 2 A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+
+    bool a[n + 1];
+    for (int i = 1; i <= n; i++)
+    {
+        a[i] = (s[i - 1] == '1');
+    }
+
+    int cost[n + 1];
+
+    for (int i = n; i >= 1; i--)
+    {
+        for (int j = i; j <= n; j += i)
+        {
+            if (a[j]) break;
+            cost[j] = i;
+        }
+    }
+
+    ll ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (!a[i])
+            ans += cost[i];
+    }
+
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
